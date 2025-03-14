768f719cf64deb844b14d7f393977d553e616ac7
Rudolfs <ruudisrudolfs@gmail.com>
2025-03-14 15:51:53 +0200

    updated jwt token

--- server/server.js
+++ server/server.js
@@ -31,20 +31,21 @@ mongoose
 const app = express();
 
 // CORS configuration
+const allowedOrigins = [
+    "http://localhost:5173", // For local development
+    "https://campingjjweb-2.onrender.com", // Your frontend URL
+];
+
 app.use(
     cors({
         origin: (origin, callback) => {
-            const allowedOrigins = [
-                "http://localhost:5173",
-                "https://campingjjweb-2.onrender.com",
-            ];
-            if (allowedOrigins.includes(origin)) {
+            if (allowedOrigins.includes(origin) || !origin) {
                 callback(null, true); // Allow the request
             } else {
                 callback(new Error("Not allowed by CORS")); // Block the request
             }
         },
-        credentials: true, // Allow cookies and credentials
+        credentials: true, // Allow credentials (cookies)
     })
 );
 // Parse JSON requests
